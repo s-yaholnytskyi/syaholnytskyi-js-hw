9f11055fb26042209b28f8c45685b5846d679731
Serhii Yaholnytskyi <Serhii.Yaholnytskyi@harmonicinc.com>
2020-04-29 19:09:35 +0300

    homework1

---
+++ homework-01/task-6.js
@@ -0,0 +1,13 @@
+let message = 1
+let total = 0
+
+while (true) {
+    message = prompt('Enter a number')
+    if (message === null) {
+        break
+    } else {
+    total += parseInt(message)
+    }
+}
+
+alert(`Total sum is ${total}`)
